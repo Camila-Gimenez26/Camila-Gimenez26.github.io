537a4d73716cbc5adb979bfec4f7a779021a787b
Camila-Gimenez26 <144200107+Camila-Gimenez26@users.noreply.github.com>
2023-09-12 09:27:43 -0400

    Calculadora Funcional

--- script.js
+++ script.js
@@ -5,15 +5,52 @@ const INPUT = document.getElementById("peso");
 const BOTON = document.getElementById("calcular");
 
 BOTON.addEventListener("click", () => {
-    let peso = INPUT.value;
+ let peso = INPUT.valueAsNumber;
 
-    if (peso > 0) {
+    if (peso > 0 && peso <= 30) {
       ERROR.style.display = "none";
+
       let res = holliday(peso);
-      console.log(res)
+      let mant = (res/24)*1.5;
+      FLU.innerHTML = (res/24) + "  cc/hr";
+      FLU.style.display = "block";
+      MAN.innerHTML = "m+m/2  " + mant + "  cc/hr";
+      MAN.style.display = "block";
+    } else if (peso > 30) {
+        ERROR.style.display = "none";
+
+        let respuesta = Superf_Corporal(peso);
+        let mante = (respuesta * 2000)
+        FLU.innerHTML = respuesta * 1500  + " diario " + (respuesta*1500)/24 + " cc/hr";
+        FLU.style.display = "block";
+        MAN.innerHTML = mante +" diario " + mante/24 +" cc/hr";
+        MAN.style.display = "block";
+        
+        /*let flujo = Superf_Corporal(peso) //* 1500;
+        let man = Superf_Corporal (peso) //* 2000;
+        FLU.innerHTML = flujo + " cc/hr";
+        FLU.style.display = "block";
+        MAN.innerHTML = "m+m/2 " + flujo +" cc/hr";
+        MAN.style.display = "block";
+
+        /*let flujo = ((peso * 4) + 7 ) / (peso + 90);
+        console.log = flujo 
+        FLU.innerHTML = flujo * 1500 + " cc/hr";
+        FLU.style.display = "block";
+        MAN.innerHTML = "m+m/2 " + flujo * 2000 +" cc/hr";
+        MAN.style.display = "block";
+        /*let res = Superf_Corporal(peso * 1500);
+        let mant = (res)*2000;
+        FLU.innerHTML = (res) + " cc/hr";
+        FLU.style.display = "block";
+        MAN.innerHTML = "m+m/2 "+ mant + " cc/hr";
+        MAN.style.display = "block"
+        console.log(res)*/
     } else {
       console.log("error");
       ERROR.style.display = "block";
+      FLU.style.display = "none";
+      MAN.style.display = "none";
     }
 });
 
@@ -27,6 +64,15 @@ function holliday (peso) {
         volumen = 1500 + (peso-20)*20
     }
     return volumen;
+}
+
+function Superf_Corporal (peso) {
+    let flujo; 
+    if (peso > 30) {
+        flujo = ((peso * 4) + 7 ) /( peso + 90)
+    }
+    return flujo;
+
 }
 
 
